a8c17f5dbac4c9b170cab26cbef03cfbe07e224e
Angelica dos Santos <santosangelicassp@gmail.com>
2022-06-23 11:52:42 -0300

    adding some views

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBAction func changeNumber(_ sender: UISlider) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let storyPrompt = StoryPromptEntry()
