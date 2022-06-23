9741b3e9de07d40bc815838ede548699dcff8abc
Angelica dos Santos <santosangelicassp@gmail.com>
2022-06-23 10:27:18 -0300

    adding models

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let storyPrompt = StoryPromptEntry()
+        storyPrompt.noun = "toaster"
+        storyPrompt.adjective = "smelly"
+        storyPrompt.verb = "burps"
+        storyPrompt.number = 10
+        print(storyPrompt)
     }
-
-
 }
 
