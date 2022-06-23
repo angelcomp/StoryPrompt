ba151bbdcc62a84ab62a24824ba6af842d14d7a1
angelicaciandt <angelicas@ciandt.com>
2022-06-23 09:12:03 -0300

    Initial Commit

---
+++ StoryPrompt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by Angelica dos Santos on 23/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
